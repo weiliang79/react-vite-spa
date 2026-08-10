8f123e2050563cf5076a93c6f34a421fe10ea57b
Wei Liang <liang2010@hotmail.my>
2026-08-11 00:14:27 +0800

    Change BrowserRouter basename to root path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/react-vite-spa/"}>
+    <BrowserRouter basename={"/"}>
       <App />
     </BrowserRouter>
   </StrictMode>
